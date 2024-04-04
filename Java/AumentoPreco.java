58d99ec748b849bb069741f804f132d344cccbbd
Vitoriapm <155494901+Vitoriapm@users.noreply.github.com>
2024-04-04 19:53:28 -0300

    Create AumentoPreco.java

---
+++ Java/AumentoPreco.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AumentoPreco{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("---------AUMENTO DO PREÇO---------");
+        
+        System.out.print("Digite o valor de um produto: ");
+        double produto = sc.nextInt();
+        
+        double precoFinal = ((produto/100) * 25) + produto;
+        
+        System.out.print("O produto sofreu um aumento de 25%, logo seu valor é: R$" + precoFinal);
+    }
+}
