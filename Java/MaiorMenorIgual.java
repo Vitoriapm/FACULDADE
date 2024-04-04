8cd14db83315d292c1751fa36c9c9d09b8168a78
Vitoriapm <155494901+Vitoriapm@users.noreply.github.com>
2024-04-04 20:12:25 -0300

    Create MaiorMenorIgual.java

---
+++ Java/MaiorMenorIgual.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MaiorMenorIgual{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("---------COMPARAÇÃO DE VALORES---------");
+
+        
+        System.out.print("Digite o primeiro valor: ");
+        int a = sc.nextInt();
+        
+        System.out.print("Digite o segundo valor: ");
+        int b = sc.nextInt();
+        
+        int maior = a, menor = b;
+        
+        if(a > b){
+            maior = a;
+            menor = b;
+            System.out.print(maior + " é MAIOR do que " + menor);
+        }else if(a < b){
+            menor = a;
+            maior = b;
+            System.out.print(menor + " é MENOR do que " + maior);
+        }else{
+            System.out.print(maior + " é IGUAL a " + menor);
+        }
+    }
+}
