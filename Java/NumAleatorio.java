118d7095e82e4406db16c32a63efb3a5278ccc0b
Vitoriapm <155494901+Vitoriapm@users.noreply.github.com>
2024-04-05 11:36:09 -0300

    Update NumAleatorio.java

--- Java/NumAleatorio.java
+++ Java/NumAleatorio.java
@@ -1,3 +1,9 @@
+/* Crie um jogo onde o computador gera um número aleatório entre 1 e 100, e o usuário tenta advinhar o número.
+Utilize um laço do-while para permitir que o usuário continue tentando até que ele advinhe corretamente o número gerado.
+Se errar, informe ao usuário se o número digitado é maior ou menor do valor a ser advinhado.
+OBS: Para utilizar a classe RANDOM no Java, utilize: import java.util.Random; Random Rand = new Random();
+int numeroAleat = rand.nextInt(101)+1; */
+
 import java.util.Scanner;
 import java.util.Random;
 
