1e6627dc59d39ba58b84bcb78f6c6bd29bc6fce8
Vitoriapm <155494901+Vitoriapm@users.noreply.github.com>
2024-05-07 17:30:15 -0300

    Update Exibir.java

--- CRUD/metodos/Exibir.java
+++ CRUD/metodos/Exibir.java
@@ -1,14 +1,15 @@
-package crud;
+package metodos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
-public class Exibir{
+public class Editar {
 
-	public static void exibirDados() throws SQLException{
+	public static void editar() throws SQLException{
+		Scanner sc = new Scanner(System.in);
 	
 		//url de conexao
 		String url = "jdbc:mysql://localhost:3306/crud";
@@ -18,22 +19,43 @@ public class Exibir{
 		Connection conexao = null;
 		conexao = DriverManager.getConnection(url, user, password);
 		
-		//código mysql para inserção
-		String sql = "SELECT * FROM empresa";
-
-		Statement st = conexao.createStatement();
-		ResultSet rs = st.executeQuery(sql);
+		//código mysql para update
+		String sql = "UPDATE empresa "
+				+ "SET cnpj = ?, nome = ?, endereco = ?, email = ?"
+				+ "WHERE cnpj = ?";
+	
+		PreparedStatement st = conexao.prepareStatement(sql);
 		
-		System.out.println("--------------EMPRESAS--------------");
-		while(rs.next()) {
-			System.out.println("[CNPJ: " + rs.getString("cnpj" ) 
-				+ ", Nome: " + rs.getString("nome") 
-				+ ", Endereço: " + rs.getString("endereco") 
-				+ ", E-mail: " + rs.getString("email") + "]");
-			System.out.println(" ");
-		}
+		//atualizar os valores
+		System.out.println("Qual o CNPJ da empresa que você deseja atualizar? ");
+		String escolhaCnpj = sc.nextLine();
+		
+		System.out.println("CNPJ: ");
+		String cnpj = sc.nextLine();
+		
+		System.out.println("Nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.println("Endereço: ");
+		String endereco = sc.nextLine();
+		
+		System.out.println("E-mail: ");
+		String email = sc.nextLine();
+		
+		
+		//adicionar os valores no banco
+		st.setString(1, cnpj);
+		st.setString(2, nome);
+		st.setString(3, endereco);
+		st.setString(4, email);
+		st.setString(5, escolhaCnpj);
+		st.executeUpdate();
+		
+		System.out.println("Os dados foram atualizados com sucesso!");
 		
 		conexao.close();
-		
+		sc.close();
 	}
+	
+	
 }
